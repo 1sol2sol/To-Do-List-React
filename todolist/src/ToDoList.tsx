2dcd4c1013ff40c9e0e93dcba0f08a18e3804c25
1sol2sol <17gksthf@gmail.com>
2022-08-23 00:52:47 +0900

    Set Errors for validation

--- todolist/src/ToDoList.tsx
+++ todolist/src/ToDoList.tsx
@@ -38,42 +38,66 @@ interface IForm {
   username: string;
   password: string;
   password1: string;
+  extraError?:string;
 }
 
 function ToDoList(){
-  const {register, handleSubmit, formState:{errors}} = useForm<IForm>({
+  const {register, handleSubmit, formState:{errors}, setError} = useForm<IForm>({
     defaultValues:{
       email:"@naver.com",
     }
   });
-  const onValid = (data:any) => {
-    console.log(data);
+  const onValid = (data:IForm) => {
+    if(data.password !== data.password1){
+      setError("password1",
+      {message:"Password are not the same"},
+      {shouldFocus:true}
+      )
+    }
+    // setError("extraError",{message:"Server offline."})
   }  
-
+  console.log(errors);
+  
   return (
     <div>
       <form style={{display:"flex", flexDirection: "column"}} onSubmit={handleSubmit(onValid)}>
+        {/* email */}
         <input {...register("email",{required: "email required", 
         pattern: {
           value: /^[A-Za-z0-9._%+-]+@naver.com$/,
           message: "Only naver.com emails allowed",
         },
-        })} placeholder="Email" />
+        })} 
+        placeholder="Email" 
+        />
         <span>
           {errors?.email?.message}
         </span>
-        <input {...register("first_name",{required: "First Name is required"})} placeholder="First Name" />
+        {/* First Name */}
+        <input 
+          {...register("first_name",{
+              required: "First Name is required", 
+              validate:{
+                noNico: (value) => value.includes("nico") ? "nicos are not allowed" : true,
+                noNick: (value) => value.includes("nick") ? "nicks are not allowed" : true,
+            }
+          })} 
+          placeholder="First Name" 
+          />
         <span>
           {errors?.first_name?.message}
         </span>
+        {/* Last Name */}
         <input {...register("last_name",{required: "Last Name is required"})} placeholder="Last Name" />
         <span>
           {errors?.last_name?.message}
         </span>
-        <input {...register("username",{required: "username is required", minLength:10})} placeholder="Username" />
+        {/* username */}
+        <input {...register("username",{required: "username is required", minLength:5})} placeholder="Username" />
         <span>
           {errors?.username?.message}
         </span>
+        {/* password */}
         <input {...register("password",{required: "Password is required", minLength:{
           value: 5,
           message: "Your password is too short.",
@@ -81,11 +105,14 @@ function ToDoList(){
           <span>
           {errors?.password?.message}
         </span>
+        {/* password check */}
         <input {...register("password1",{required: "Password is required", minLength:5})} placeholder="Password1" />
         <span>
           {errors?.password1?.message}
         </span>
         <button>Add</button>
+        { /* 물음표를 붙이면, 그 항목이 undefined면 그 뒤를 실행하지 않음  */ }
+        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
